c02c18d2d8a7688cade9414ef46bc8c78c2099ef
Franciele Aires <f.aires2016@gmail.com>
2025-03-26 11:50:02 -0300

    feat: adicionando validadores de atualização para VetCare, Procedure e Animal

---
+++ CliniCare.Application/Validators/UpdateAnimalValidator.cs
@@ -0,0 +1,33 @@
+﻿using CliniCare.Application.Commands.Animals;
+using CliniCare.Domain.Models;
+using FluentValidation;
+using System;
+
+namespace CliniCare.Application.Validators
+{
+    public class UpdateAnimalValidator : AbstractValidator<UpdateAnimalCommand>
+    {
+        public UpdateAnimalValidator()
+        {
+            RuleFor(a => a.Species)
+                .MaximumLength(50)
+                .WithMessage("Espécie não pode exceder 50 caracteres.");
+
+            RuleFor(a => a.Name)
+                .MaximumLength(50)
+                .WithMessage("Nome não pode exceder 50 caracteres.");
+
+            RuleFor(a => a.Breed)
+                .MaximumLength(50)
+                .WithMessage("Raça não pode exceder 50 caracteres.");
+
+            RuleFor(a => a.Gender)
+                .Must(g => g == "Macho" || g == "Fêmea")
+                .WithMessage("Sexo deve ser 'Macho' ou 'Fêmea'.");
+
+            RuleFor(a => a.DateOfBirth)
+                .LessThanOrEqualTo(DateTime.Today)
+                .WithMessage("Data de nascimento deve ser até hoje.");
+        }
+    }
+}
---
+++ CliniCare.Application/Validators/UpdateProcedureValidator.cs
@@ -0,0 +1,24 @@
+﻿using CliniCare.Application.Commands.Procedures;
+using FluentValidation;
+
+public class UpdateProcedureValidator : AbstractValidator<UpdateProcedureCommand>
+{
+    public UpdateProcedureValidator()
+    {
+
+        RuleFor(vp => vp.ServiceName)
+            .MaximumLength(100)
+            .WithMessage("Nome do serviço não pode exceder 100 caracteres.")
+            .When(vp => !string.IsNullOrEmpty(vp.ServiceName));
+
+        RuleFor(vp => vp.Price)
+            .GreaterThan(0)
+            .WithMessage("Preço deve ser maior que zero.")
+            .When(vp => vp.Price > 0);  
+
+        RuleFor(vp => vp.Description)
+            .MaximumLength(500)
+            .WithMessage("Descrição não pode exceder 500 caracteres.")
+            .When(vp => !string.IsNullOrEmpty(vp.Description)); 
+    }
+}
